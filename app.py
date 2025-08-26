c9c05e4ad88031d83951322f67f84631d67c84c8
sou23-git <sooou23ohata@gmail.com>
2025-08-26 14:03:52 +0900

    とりあえずオウム返しテスト実装

--- app.py
+++ app.py
@@ -1,1 +1,101 @@
-#make git repository!
+# -*- coding: utf-8 -*-
+
+from flask import Flask, request, abort
+from linebot.v3 import WebhookHandler
+from linebot.v3.exceptions import InvalidSignatureError
+from linebot.v3.messaging import (
+	ApiClient, Configuration, MessagingApi,
+	ReplyMessageRequest, PushMessageRequest,
+	TextMessage, PostbackAction
+)
+from linebot.v3.webhooks import (
+	FollowEvent, MessageEvent, PostbackEvent, TextMessageContent
+)
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+#環境変数を変数に代入
+CHANNEL_ACCESS_TOKEN = os.environ["CHANNEL_ACCESS_TOKEN"]
+CHANNELSECRET = os.environ["CHANNEL_SECRET"]
+
+#Flaskアプリのインスタンス化
+app = Flask(__name__)
+
+#LINEのアクセストークンの読み込み
+configuration = Configuration(access_token=CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(CHANNELSECRET)
+
+#コールバック（おまじない）
+@app.route("/callback", methods=['POST'])
+def callback(): 
+    #get X-Line-Signature header calue
+    signature = request.headers['X-Line-Signature']
+    
+    #get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    
+    #handle webhook body
+    try: 
+        handler.handle(body, signature)
+    except InvalidSignatureError: 
+        app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+    
+    return 'OK'
+
+
+
+#友達追加時のハンドラ
+@handler.add(FollowEvent)
+def handle_follow(event): 
+    #APIインスタンス化
+    with ApiClient(configuration) as api_client: 
+        line_bot_api = MessagingApi(api_client)
+    
+    #返信
+    line_bot_api.reply_message(ReplyMessageRequest(
+        replyToken=event.reply_token, 
+        messages=[TextMessage(text='Thank You!')]
+    ))
+
+
+#返信ハンドラ
+@handler.add(MessageEvent, message=TextMessageContent)
+def handle_message(event): 
+    #APIインスタンス化
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+    
+    #受信メッセージの中身を確認
+    received_message = event.message.text
+    
+    #APIを呼んで送信者のプロフィールを取得
+    profile = line_bot_api.get_profile(event.sourse.user_id)
+    display_name = profile.display_name
+    
+    #返信メッセージ
+    reply = f'{display_name}産のメッセージ\n{received_message}'
+    
+    #オウム返し
+    line_bot_api.reply_message(ReplyMessageRequest(
+        replyToken=event.reply_token, 
+        messages=[TextMessage(text=reply)]
+    ))
+
+
+## 起動確認用ウェブサイトのトップページ
+@app.route('/', methods=['GET'])
+def toppage():
+	return 'Hello world!'
+
+
+
+
+
+#実行
+if __name__ == "__main__": 
+    #ローカルテスト用に'debug=True'に
+    app.run(host="0.0.0.0", port=8000, debug=True)
